972c87e4aefb4bb6b4c2c9364b44275fed230fa4
Wil Simpson <simpson.wil255@gmail.com>
2022-05-26 17:11:15 -0500

    Added agones plugin

--- Source/SRO/SRO.Build.cs
+++ Source/SRO/SRO.Build.cs
@@ -8,6 +8,14 @@ public class SRO : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"Agones",
+		});
 	}
 }
