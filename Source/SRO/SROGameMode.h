7d373d92287eb1fd2d6a39a0d43ee7f832257c52
Wil Simpson <simpson.wil255@gmail.com>
2022-05-26 15:30:03 -0500

    Initial commit

---
+++ Source/SRO/SROGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SROGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASROGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASROGameMode();
+};
+
+
+
