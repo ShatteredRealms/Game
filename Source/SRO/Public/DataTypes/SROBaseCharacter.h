87c540b2dbaf709078f8c49ae2258359ce30072f
Wil Simpson <simpson.wil255@gmail.com>
2022-05-27 13:41:16 -0500

    Refactored OfflineCharacter to BaseCharacter

---
+++ Source/SRO/Public/DataTypes/SROBaseCharacter.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "UObject/ObjectMacros.h"
+#include "SROGender.h"
+#include "SRORealm.h"
+#include "SROBaseCharacter.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSROBaseCharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Id;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Level;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSROGender Gender;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSRORealm Realm;
+};
