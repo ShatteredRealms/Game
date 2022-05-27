7405b3b8d7c0d64eb3ca934c1fa320b88e6be607
Wil Simpson <simpson.wil255@gmail.com>
2022-05-27 01:23:10 -0500

    Initialized GameMode components. Added Agones SDK.

--- Source/SRO/SROGameMode.cpp
+++ Source/SRO/SROGameMode.cpp
@@ -2,6 +2,11 @@
 
 #include "SROGameMode.h"
 #include "SROCharacter.h"
+#include "SROGameSession.h"
+#include "SROGameState.h"
+#include "SROPlayerController.h"
+#include "SROPlayerState.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
 
 ASROGameMode::ASROGameMode()
@@ -12,4 +17,28 @@ ASROGameMode::ASROGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	PlayerStateClass = ASROPlayerState::StaticClass();
+
+	PlayerControllerClass = ASROPlayerController::StaticClass();
+
+	// GameStateClass = ASROGameState::StaticClass();
+
+	GameSessionClass = ASROGameSession::StaticClass();
+
+	AgonesSDK = CreateDefaultSubobject<UAgonesComponent>(TEXT("AgonesSDK"));
+
+	FBase64::Decode(FPlatformMisc::GetEnvironmentVariable(TEXT("JWT_PUBLIC_KEY")), JWTPublicKey);
+	FBase64::Decode(FPlatformMisc::GetEnvironmentVariable(TEXT("JWT_PRIVATE_KEY")), JWTPrivateKey);
 }
+
+UAgonesComponent* ASROGameMode::GetAgonesSDK(UObject* WorldContextObject)
+{
+	if (const ASROGameMode* GameMode = Cast<ASROGameMode>(UGameplayStatics::GetGameMode(WorldContextObject)))
+	{
+		return GameMode->AgonesSDK;
+	}
+
+	return nullptr;
+}
+
--- Source/SRO/SROGameMode.h
+++ Source/SRO/SROGameMode.h
@@ -3,17 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AgonesComponent.h"
 #include "GameFramework/GameModeBase.h"
 #include "SROGameMode.generated.h"
 
-UCLASS(minimalapi)
+UCLASS(minimalapi, Config = "GameMode")
 class ASROGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
-public:
-	ASROGameMode();
-};
+private:
+	/** The JWT public key used to verify JWT tokens from SRO services */
+	UPROPERTY(Config)
+	FString JWTPublicKey;
 
+	/**
+	 * The JWT private key used to sign JWT tokens for authentication and authorization between
+	 * SRO services
+	 */
+	UPROPERTY(Config)
+	FString JWTPrivateKey;
+	
+public:
+	/** Agones SDK */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UAgonesComponent* AgonesSDK;
+
+	/** Default constructor that setups the base components */
+	ASROGameMode();
+
+	/** Gets the AgonesSDK from */
+	UFUNCTION(BlueprintCallable)
+	static UAgonesComponent* GetAgonesSDK(UObject* WorldContextObject);
+};
 
 
