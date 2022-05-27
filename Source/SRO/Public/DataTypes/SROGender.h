8dc4b8fb5482e8aaea7a978d17a612a1e547f52b
Wil Simpson <simpson.wil255@gmail.com>
2022-05-27 02:11:55 -0500

    Added breed and race

---
+++ Source/SRO/Public/DataTypes/SROGender.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "UObject/ObjectMacros.h"
+#include "SROGender.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSROGender
+{
+	GENERATED_BODY()
+
+	/** The database ID */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 Id;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString Name;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString Description;
+};
---
+++ Source/SRO/Public/DataTypes/SRORace.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "UObject/ObjectMacros.h"
+#include "SRORace.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSRORace
+{
+	GENERATED_BODY()
+
+	/** The database ID */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 Id;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString Name;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString Description;
+};
